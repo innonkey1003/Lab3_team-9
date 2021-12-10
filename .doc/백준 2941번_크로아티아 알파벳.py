cd8966bc29b476c3636b86e52bc4108bbfadd2eb
innonkey1003 <93446072+innonkey1003@users.noreply.github.com>
2021-12-10 21:33:45 +0900

    Delete 백준 2941번_크로아티아 알파벳.py

--- .doc/백준 2941번_크로아티아 알파벳.py
+++
@@ -1,8 +0,0 @@
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-
-giveword = input("예제를 입력하세요: ")
-
-for alphabet in croatia:
-    giveword = giveword.replace(alphabet, '!')
-
-print("크로아티아 알파벳 개수 : ", len(giveword))
